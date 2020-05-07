261bd8916f54ad6c4caeeab06576718401b2f98f
Zolbayar orshil <zolbayarorshil@Zolbayars-MacBook-Pro-2.local>
2020-05-06 18:33:44 -0600

    finished the jquery lab

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("test");
+
+$(document).ready(function() {
+    $("#addClick").click(function() {
+    let newInt = $("#newInput").val();
+      $("#addHere").append("<li class='newList'><button class='remove'>X</button> " +newInt+'</li>');
+      $(".remove").click(function() {
+        $(".newList").fadeOut(2000);
+      });
+    });
+  });
